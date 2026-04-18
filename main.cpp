6e4285a961b1ba25571f6f0f99ecb6ba490b0bf8
Theshakyhorse <amaximus.sierra@gmail.com>
2026-04-17 20:46:56 -0700

    testing 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 // COMSC-210 | Lab 30 | Alexander Sierra
+//I changed the text files because I realized they
+//don't have regions associated with them
 #include <iostream>
 using namespace std;
 
 int main() {
-
+    
     return 0;
 }
